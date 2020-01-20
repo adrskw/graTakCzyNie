91ee2bd022527cb33a140fdcdcc322c6f2a64ddb
Krzysztof Ryś <riccoo1784@gmail.com>
2020-01-20 17:59:47 +0100

    Added Field enum

---
+++ graTakCzyNieLibrary/Field.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graTakCzyNieLibrary
+{
+    public enum Field
+    {
+        Start,
+        Meta,
+        Question,
+        Penalty,
+        Bonus,
+        Trap,
+        Normal
+    }
+
+
+}
