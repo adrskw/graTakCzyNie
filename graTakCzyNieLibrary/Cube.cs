cb5d387af14c8fc6801a3038e653bf7f8c7c9b0a
Krzysztof Ryś <riccoo1784@gmail.com>
2020-01-20 18:42:46 +0100

    Added Cube class with random number method

---
+++ graTakCzyNieLibrary/Cube.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graTakCzyNieLibrary
+{
+    public class Cube
+    {
+        private static Random random = new Random();
+        public static async Task<int> GetRandomResult(int a, int b)
+        {
+            return await Task.Run(() =>
+            {
+                return random.Next(a, b);
+            });
+        }
+    }
+}
