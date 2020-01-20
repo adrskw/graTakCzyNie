a1684a030dd3e900f69e9c9a438b8868aa65d948
Krzysztof Ryś <riccoo1784@gmail.com>
2020-01-20 18:35:34 +0100

    Added Engine and EngineResult class with StartGame method

---
+++ graTakCzyNieLibrary/Engine.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graTakCzyNieLibrary
+{
+    public class Engine
+    {
+        private Board board;
+
+        private bool gameRunning = false;
+
+        public List<Player> PlayersList { get; private set; } = new List<Player>();
+
+        public async Task<EngineResult> StartGame()
+        {
+            return await Task.Run(() =>
+            {
+                if (PlayersList is null)
+                {
+                    return new EngineResult
+                    {
+                        Succedeed = false,
+                        ErrorMessage = "Player list is not set!"
+                    };
+                }
+                else if (gameRunning)
+                {
+                    return new EngineResult
+                    {
+                        Succedeed = false,
+                        ErrorMessage = "Game is already running!"
+                    };
+                }
+                else
+                {
+                    qDatabase = new QuestionDatabase();
+                    board = new Board(50);
+                    return new EngineResult
+                    {
+                        Succedeed = true
+                    };
+                }
+            });
+        }
+    }
---
+++ graTakCzyNieLibrary/EngineResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graTakCzyNieLibrary
+{
+    class EngineResult
+    {
+        public bool Succedeed { get; set; }
+        public Field Field { get; set; }
+        public Player Player { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
