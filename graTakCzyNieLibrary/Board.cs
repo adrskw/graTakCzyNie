2c9a696328c0c38f5de2a0acc8c957861175ae4f
adrskw <48623654+adrskw@users.noreply.github.com>
2020-01-22 00:03:45 +0100

    Creating board error fixed

--- graTakCzyNieLibrary/Board.cs
+++ graTakCzyNieLibrary/Board.cs
@@ -19,6 +19,8 @@ namespace graTakCzyNieLibrary
 
         private async void CreateBoard(int fieldNumber)
         {
+            CreatedBoard = null;
+
             if (CreatedBoard == null)
             {
                 CreatedBoard = new Dictionary<int, Field>() { { 0, Field.Start } };
@@ -52,5 +54,6 @@ namespace graTakCzyNieLibrary
                 return field;
             });
         }
+
     }
 }
