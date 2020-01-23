b29bd526c7328346b9a4842ff3c15eeb688578d4
Konrad Sęk <konrad.sek@microsoft.wsei.edu.pl>
2020-01-23 19:38:58 +0100

    Added tests with some test methods - class Engine

---
+++ graTakCzyNieTests/EngineTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using graTakCzyNieLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace graTakCzyNie.Tests
+{
+    [TestClass]
+    public class EngineTests
+    {
+        Engine engine = new Engine();
+        List<Player> playerList = new List<Player>();
+
+        [TestMethod]
+        public async Task If_CreatePlayersSuccedeed_ReturnTrue()
+        {
+            playerList.Add(new Player(1, "Gracz", "Red"));
+            var result = await engine.CreatePlayers(playerList);
+            Assert.IsTrue(result.Succedeed);
+        }
+
+        [TestMethod]
+        public async Task If_PlayerNameAlreadyExists_ReturnFalse()
+        {
+            playerList.Add(new Player(1, "Gracz", "Red"));
+            playerList.Add(new Player(1, "Gracz", "Red"));
+            var result = await engine.CreatePlayers(playerList);
+            Assert.IsFalse(result.Succedeed);
+        }
+
+        [TestMethod]
+        public async Task If_StartGameAndPlayersListIsNotSet_ReturnFalse()
+        {
+            var result = await engine.StartGame(50);
+            Assert.IsFalse(result.Succedeed);
+        }
+
+        [TestMethod]
+        public async Task If_StartGameAndPlayerListIsSet_ReturnTrue()
+        {
+            playerList.Add(new Player(1, "Gracz", "Red"));
+            await engine.CreatePlayers(playerList);
+
+            var result = await engine.StartGame(50);
+
+            Assert.IsTrue(result.Succedeed);
+        }
+    }
+}
---
+++ graTakCzyNieTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("graTakCzyNieTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("graTakCzyNieTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ce29f511-7252-4076-98af-3714673d5d14")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
