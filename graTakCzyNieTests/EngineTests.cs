caf2bb5308f517ad752b5f609a8b6db571e2f412
Konrad Sęk <konrad.sek@microsoft.wsei.edu.pl>
2020-01-25 17:36:51 +0100

    Added some tests

--- graTakCzyNieTests/EngineTests.cs
+++ graTakCzyNieTests/EngineTests.cs
@@ -46,5 +46,92 @@ namespace graTakCzyNie.Tests
 
             Assert.IsTrue(result.Succedeed);
         }
+
+        [TestMethod]
+        public async Task If_TryingMovePlayerWhenGameIsNotRunning_ReturnFalse()
+        { 
+            var result = await engine.Move(new Player(2, "Test", "Green"), 1);
+            Assert.IsFalse(result.Succedeed);
+        }
+
+        [TestMethod]
+        public async Task If_TryingMovePlayerWhoIsNotOnPlayersList_ReturnFalse()
+        {
+            playerList.Add(new Player(1, "Gracz", "Red"));
+            await engine.CreatePlayers(playerList);
+            await engine.StartGame(50);
+
+            var result = await engine.Move(new Player(2, "Test", "Green"), 1);
+
+            Assert.IsFalse(result.Succedeed);
+        }
+
+        [TestMethod]
+        public async Task If_TryingMovePlayerWhoIsInTrap_ReturnFalse()
+        {
+            var player = new Player(1, "Gracz", "Red");
+            playerList.Add(player);
+            await engine.CreatePlayers(playerList);
+            await engine.StartGame(50);
+            engine.PlayersList[0].ImprisonedTo = 3;
+            var result = await engine.Move(player, 5);
+
+            Assert.IsFalse(result.Succedeed);
+        }
+
+
+        [TestMethod]
+        public async Task If_TryingMovePlayerSuccedeed_ReturnTrue()
+        {
+            var player = new Player(1, "Gracz", "Red");
+            playerList.Add(player);
+            await engine.CreatePlayers(playerList);
+            await engine.StartGame(50);
+            var result = await engine.Move(player, 5);
+
+            Assert.IsTrue(result.Succedeed);
+        }
+
+
+        [TestMethod]
+        public async Task If_PlayerDoNotExistsOnList_PlayerAnswer_ReturnFalse()
+        {
+            var player = new Player(1, "Gracz", "Red");
+            playerList.Add(player);
+            var player2 = new Player(2, "Gracz2", "Red");
+            await engine.CreatePlayers(playerList);
+            await engine.StartGame(50);
+
+            var result = await engine.PlayerAnswer(player2, 1, false);
+
+            Assert.IsFalse(result.Succedeed);
+        }
+
+        [TestMethod]
+        public async Task If_PlayerAnswerIsWrong_ReturnCorrectAnswerFalse()
+        {
+            var player = new Player(1, "Gracz", "Red");
+            playerList.Add(player);
+            await engine.CreatePlayers(playerList);
+            await engine.StartGame(50);
+
+            var result = await engine.PlayerAnswer(player, 1, false);
+
+            Assert.IsFalse(result.Question.CorrectAnswer.Value);
+        }
+
+        [TestMethod]
+        public async Task If_PlayerAnswerIsCorrect_ReturnCorrectAnswerTrue()
+        {
+            var player = new Player(1, "Gracz", "Red");
+            playerList.Add(player);
+            await engine.CreatePlayers(playerList);
+            await engine.StartGame(50);
+
+            var result = await engine.PlayerAnswer(player, 1, true);
+
+            Assert.IsTrue(result.Question.CorrectAnswer.Value);
+        }
+
     }
 }
